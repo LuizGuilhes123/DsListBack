003fadcdac8bcbe323c3836ee89ef52d63bd1080
LuizGuilhes123 <luizguilhermeinformat@gmail.com>
2025-01-22 08:50:52 -0300

    feat: foi atualizado as classes referentes a games com o uso do lombok #1

--- src/main/java/com/devsuperior/dslist/dto/GameListDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameListDTO.java
@@ -1,22 +1,17 @@
 package com.devsuperior.dslist.dto;
 
-import com.devsuperior.dslist.entities.GameList;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class GameListDTO {
 
 	private Long id;
 	private String name;
-	
-	public GameListDTO(GameList entity) {
-		id = entity.getId();
-		name = entity.getName();
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public String getName() {
-		return name;
-	}
 }
