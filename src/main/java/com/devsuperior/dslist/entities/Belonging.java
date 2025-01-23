8e5cafa245c3c874faeeea4eab15732247cf7563
LuizGuilhes123 <luizguilhermeinformat@gmail.com>
2025-01-23 10:26:35 -0300

    feat :foi atualizado os códigos Beloging com o lombok #3

--- src/main/java/com/devsuperior/dslist/entities/Belonging.java
+++ src/main/java/com/devsuperior/dslist/entities/Belonging.java
@@ -1,12 +1,18 @@
 package com.devsuperior.dslist.entities;
 
-import java.util.Objects;
-
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "tb_belonging")
 public class Belonging {
 
@@ -15,44 +21,4 @@ public class Belonging {
 
 	private Integer position;
 
-	public void setGame(Game game) {
-		id.setGame(game);
-	}
-
-	public Game getGame() {
-		return id.getGame();
-	}
-
-	public void setList(GameList list) {
-		id.setList(list);
-	}
-
-	public GameList getList() {
-		return id.getList();
-	}
-
-	public Integer getPosition() {
-		return position;
-	}
-
-	public void setPosition(Integer position) {
-		this.position = position;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Belonging other = (Belonging) obj;
-		return Objects.equals(id, other.id);
-	}
 }
